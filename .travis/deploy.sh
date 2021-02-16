db2ff21c82f25b98e1fb99431e4aedf14301769e
Aleksandr Zvyagin <aleksandrzvyagin@yandex.ru>
2021-02-16 23:39:38 +0300

    Настройка деплоя проекта

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,7 @@
 #sudo chmod 600 ./.travis/deploy # Allow read access to the private key
 #ssh-add ./.travis/deploy # Add the private key to SSH
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ~/.ssh/deploy deploy@$IP -p $PORT <<EOF
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ~/.ssh/deploy $USER@$IP -p $PORT <<EOF
   cd $DEPLOY_PATH
   git pull
 EOF
