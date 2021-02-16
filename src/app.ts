fa146cb74f9fa60d11c86a0bf9ceba33a2a90397
Aleksandr Zvyagin <aleksandrzvyagin@yandex.ru>
2021-02-16 23:37:36 +0300

    Настройка деплоя проекта

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from "express";
 
 const app = express();
-const port = 8000;
+const port = 8080;
 app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
+  res.send("Update");
 });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
