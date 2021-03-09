6e3a6b4a184a3b4532da12de92dbf2dbf4e67844
Aleksandr Zvyagin <aleksandrzvyagin@yandex.ru>
2021-03-09 18:38:59 +0300

    Настройка деплоя проекта

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-#sudo chmod 600 ./.travis/deploy # Allow read access to the private key
-#ssh-add ./.travis/deploy # Add the private key to SSH
-# Skip this command if you don't need to execute any additional commands after deploying.
-#ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ~/.ssh/deploy $USER@$IP -p $PORT <<EOF
-#  cd $DEPLOY_PATH
-#  git pull
-#EOF
-scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ./archive.tar.gz deploy@$IP:$DEPLOY_PATH
+scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ./archive.tar.gz $USER@$IP:$DEPLOY_PATH
 echo 'copy files'
 ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ~/.ssh/deploy $USER@$IP -p $PORT <<EOF
   cd $DEPLOY_PATH
   tar -xvf archive.tar.gz
+  cd /dist
   node app.js
 EOF
