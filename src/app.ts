14ae550b5ee1f130c3d8a22bd5627bc68f6f1d54
Aleksandr Zvyagin <aleksandrzvyagin@yandex.ru>
2021-03-10 10:06:04 +0300

    Настройка конфига

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-const ejs = require("ejs").__express;
+import ejs  from "ejs";
 import express from "express";
 
 const app = express();
 app.set("view engine", "ejs");
-app.engine('.ejs', ejs);
+app.engine('.ejs', ejs.__express);
 const port = 8080;
 app.get("/", (req, res) => {
   res.send("Update");
