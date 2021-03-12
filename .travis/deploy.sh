286703ead32dfde6dae70f397fda7f8b9d9acd39
Aleksandr Zvyagin <aleksandrzvyagin@yandex.ru>
2021-03-12 11:52:26 +0300

    Настройка конфига

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ./archive.tar $USER@$IP:$DEPLOY_PATH
-ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ~/.ssh/deploy $USER@$IP -p $PORT
-cd $DEPLOY_PATH
-tar -xvf archive.tar
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ~/.ssh/deploy $USER@$IP -p $PORT <<EOF
+  cd $DEPLOY_PATH
+  tar -xvf archive.tar
+EOF
