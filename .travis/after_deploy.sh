cf9375dc77851bdc82e4ab87cda7009444d18e61
Aleksandr Zvyagin <aleksandrzvyagin@yandex.ru>
2021-03-12 16:36:52 +0300

    Настройка конфига

--- .travis/after_deploy.sh
+++ .travis/after_deploy.sh
@@ -1,4 +1,4 @@
 ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ~/.ssh/deploy $USER@$IP -p $PORT  <<EOF
   cd $DEPLOY_PATH/dist
-  node bundle.js
+  pm2 start bundle.js --watch
 EOF
--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 app.engine('.ejs', ejs.__express);
 const port = 8000;
 app.get("/", (req, res) => {
-  res.send("config");
+  res.send("config 2");
 });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
